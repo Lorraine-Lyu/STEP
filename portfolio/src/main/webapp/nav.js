367123f058ad68c54f9b1992bc104088971a3914
Lorraine Lyu <yl191@rice.edu>
2020-05-31 00:08:29 -0500

    adds js for changing iframe content

---
+++ portfolio/src/main/webapp/nav.js
@@ -0,0 +1,21 @@
+/* change src of iframe according to input index
+*/
+function load(str) {
+  var frame = document.getElementsByTagName("iframe")[0];
+  switch(str) {
+    case 1:
+      frame.src = "main.html";
+      break;
+    case 2:
+      frame.src = "introduction.html";
+      break;
+    case 3:
+      frame.src = "projects.html";
+      break;
+    case 4:
+      frame.src = "gallery.html";
+      break;
+    default:
+      frame.src = "error.html";
+  }
+}
