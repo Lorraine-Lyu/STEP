4c06d474282cd97d803f6db56a1b6b198f789ca2
Lorraine Lyu <yl191@rice.edu>
2020-06-02 14:17:58 +0000

    moves array outside of function. adds id to loadFrame

--- portfolio/src/main/webapp/nav.js
+++ portfolio/src/main/webapp/nav.js
@@ -1,14 +1,16 @@
-/** 
- * Change src of iframe according to pageIndex
- * @param {integer} pageIndex index of page the iframe should load
- */
-function loadFrame(pageIndex) {
-  const frame = document.getElementById('iframe');
+/** The List of relative paths to all files to be loaded in iframe */
   const sources = [
     'main.html', 
     'introduction.html', 
     'projects.html', 
     'gallery.html',
   ];
+
+/** 
+ * Change src of iframe according to pageIndex
+ * @param {integer} pageIndex index of page the iframe should load
+ */
+function loadFrame(pageIndex) {
+  const frame = document.getElementById('sub-page');
   frame.src = sources[pageIndex];
 }
