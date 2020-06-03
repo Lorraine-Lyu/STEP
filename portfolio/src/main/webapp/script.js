9f27ee4e28278de79e09bf2249a74052a91b7529
Lorraine Lyu <yl191@rice.edu>
2020-06-03 02:05:03 +0000

    makes hard coded string sent to frontend using fetch

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,47 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// the indexes recording the picture 
-// the slides in gallery.html is showing
-var slide1 = 0;
-var slide2 = 0;
-
 /**
- * Controls(flips) both slideshow in gallery.html
+ * fetch data from servelet
  */
-function flip(/* num of pages to flip= */ num, 
-  /* index of the slide= */ sIndex) {
-  var slide;
-  if (sIndex == 1) {
-    slide1 += num;
-    slide = document.getElementById("slide1").getElementsByTagName("img");
-    slide1 = updateFlip(slide1, slide);
-  } else {
-    slide2 += num;
-    slide = document.getElementById("slide2").getElementsByTagName("img");
-    slide2 = updateFlip(slide2, slide);
-  }
+const fetchData = async () => {
+  const response = await fetch('/data');
+  const quote = await response.text();
+  console.log("quote");
+//   document.getElementById('quote-container').innerText = quote;
 }
 
-/**
- * The helper function for flip(n,s); 
- * sets the input slide to show 
- * picture corresponding to the 'show' index
- */
-function updateFlip(/* page index= */ show, slide) {
-  if (show > 4) {
-      show = 0;
-  } else if (show < 0) {
-      show = 4;
-  }
-
-  for (i = 0; i < 5; i++) {
-    slide[i].style.display = "none";  
-  }
-  slide[show].style.display = "block";
-  return show;
-}
-
-// set both slides to the first page
-flip(0, 1);
-flip(0, 2);
+window.onload = fetchData;
