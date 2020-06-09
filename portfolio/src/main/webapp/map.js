68e8d32b77987edc7d4c1ec8770adda5ade6a76c
Lorraine Lyu <yl191@rice.edu>
2020-06-09 01:55:08 +0000

    adds markers and their onclick event handlers

--- portfolio/src/main/webapp/map.js
+++ portfolio/src/main/webapp/map.js
@@ -1,4 +1,39 @@
 const map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-      });
+  center: {lat: 43, lng: 169},
+  zoom: 2,
+});
+
+const hometownMarker = new google.maps.Marker({
+  position: {lat: 31.293463, lng: 120.666522},
+  map: map,
+  title: 'My Hometown',
+});
+
+hometownMarker.addListener('click', async () => {
+  changeContent("/suzhou");
+});
+
+const smithCollegeMarker = new google.maps.Marker({
+  position: {lat: 42.319266, lng: -72.642447},
+  map: map,
+  title: 'Smith College',
+});
+
+smithCollegeMarker.addListener('click', async () => {
+  changeContent("/smith")
+});
+
+const RiceUnivMarker = new google.maps.Marker({
+  position: {lat: 29.717296, lng: -95.399418},
+  map: map,
+  title: 'Rice University'
+});
+
+RiceUnivMarker.addListener('click', async () => {
+  changeContent("/rice")
+});
+
+const changeContent = async (path) => {
+  let content = await fetch(path);
+  document.getElementById('intro-by-place').innerHTML = content;
+}
