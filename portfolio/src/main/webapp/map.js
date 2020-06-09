ce549e2316ecee8f2d5bec013abf955d3fa56a2c
Lorraine Lyu <yl191@rice.edu>
2020-06-09 14:43:56 +0000

    adds constant variables in map.js

--- portfolio/src/main/webapp/map.js
+++ portfolio/src/main/webapp/map.js
@@ -1,6 +1,33 @@
+// The coordinates for all markers and their related properties
+const MAP_CENTER = {
+  lat: 43,
+  lng: 169,
+};
+
+const SUZHOU_COORD = {
+  lat: 31.293463,
+  lng: 120.666522,
+  title: 'My Hometown',
+  path: '/suzhou',
+};
+
+const SMITH_COORD = {
+  lat: 42.319266,
+  lng: -72.642447,
+  title: 'Smith College',
+  path: '/smith',
+};
+
+const RICE_COORD = {
+  lat: 29.717296,
+  lng: -95.399418,
+  title: 'Rice University',
+  path: '/rice',
+};
+
 // The embedded Google Map for my introduction page
 const map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 43, lng: 169},
+  center: {MAP_CENTER.lat, MAP_CENTER.lng},
   zoom: 2,
 });
 
@@ -8,33 +35,33 @@ const map = new google.maps.Map(document.getElementById('map'), {
 // Each marker has corresponding onclick event
 // which updates html content in 'intro-by-location' div
 const hometownMarker = new google.maps.Marker({
-  position: {lat: 31.293463, lng: 120.666522},
+  position: {SUZHOU_COORD.lat, SUZHOU_COORD.lng},
   map: map,
-  title: 'My Hometown',
+  title: SUZHOU_COORD.title,
 });
 
 hometownMarker.addListener('click', async () => {
-  changeContent("/suzhou");
+  changeContent(SUZHOU_COORD.path);
 });
 
 const smithCollegeMarker = new google.maps.Marker({
-  position: {lat: 42.319266, lng: -72.642447},
+  position: {SMITH_COORD.lat, SMITH_COORD.lng},
   map: map,
-  title: 'Smith College',
+  title: SMITH_COORD.title,
 });
 
 smithCollegeMarker.addListener('click', async () => {
-  changeContent("/smith")
+  changeContent(SMITH_COORD.path);
 });
 
 const RiceUnivMarker = new google.maps.Marker({
-  position: {lat: 29.717296, lng: -95.399418},
+  position: {RICE_COORD.lat, RICE_COORD.lng},
   map: map,
-  title: 'Rice University'
+  title: RICE_COORD.title,
 });
 
 RiceUnivMarker.addListener('click', async () => {
-  changeContent("/rice")
+  changeContent(RICE_COORD.path);
 });
 
 /**
