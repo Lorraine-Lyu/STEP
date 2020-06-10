e9ee3cbf727b64dbfbd004e6e355869ac44dd15b
Lorraine Lyu <yl191@rice.edu>
2020-06-10 07:16:59 +0000

    adds comments

--- portfolio/src/main/webapp/Controller.js
+++ portfolio/src/main/webapp/Controller.js
@@ -1,10 +1,24 @@
 export default class Controller {
 
-  constructor() {
+  constructor(document) {
     this.username = undefined;
     this.loggedIn = false;
+    this.document = document;
   }
 
+  openNameForm() {
+    // pops up the form with id "nameForm".
+  }
+
+  closeNameForm() {
+    // hides the form with id "nameForm".
+  }
+
+  /**
+   * Loads all comments from backend.
+   * This function is called when the page is loaded.
+   * @param {HTML div object} commentBox The div in document with id "comment-box"
+   */
   static async loadComments(commentBox) {
     let commentResponse = undefined;
     try {
@@ -28,7 +42,14 @@ export default class Controller {
     }
   }
 
-  static async checkLoginStatus(controller, document) {
+  /**
+   * Checks whether the user has logged in;
+   * if so, gets the username;
+   * if not, gets the login url.
+   * @param {Controller} controller the controller connected 
+   * to the portfolio webpage.
+   */
+  static async checkLoginStatus(controller) {
     let loginStatus = undefined;
     try {
       loginStatus = await fetch('/login');
@@ -37,14 +58,18 @@ export default class Controller {
       console.log(e);
       return;
     }
+
     try {
+      // statusObj has a boolean to indicate whether the user has logged in;
+      // and a helperInfo string which can either be username or login url.
       const statusObj = await loginStatus.json();
       if (statusObj.isUserLoggedIn) {
-        let username = loginStatus.helperInfo;
-        document.getElementById('username').textContent =  username;
+        controller.username = statusObj.helperInfo;
+        controller.document.getElementById('username').textContent = controller.username;
       } else {
-        let loginUrl = loginStatus.helperInfo;
-        document.getElementById('comment-box').innerHTML = '<p> please log in <a href="' + loginUrl + '"> here</a>';
+        let loginUrl = statusObj.helperInfo;
+        controller.document.getElementById('comment-box').innerHTML = 
+            '<p> please log in <a href="' + loginUrl + '"> here</a>';
       }
       controller.loggedIn = statusObj.isUserLoggedIn;
     } catch(e) {
