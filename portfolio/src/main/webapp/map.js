19c564d6d1eb1051fca5faa178e31c45b6c94e52
Lorraine Lyu <yl191@rice.edu>
2020-06-10 15:39:19 +0000

    changes html in js file (instead of backend)

--- portfolio/src/main/webapp/map.js
+++ portfolio/src/main/webapp/map.js
@@ -1,74 +1,90 @@
-// The coordinates for all markers and their related properties.
-const MAP_CENTER = {
-  lat: 43,
-  lng: 169,
-};
+class MapHandler {
+  // The coordinates for all markers and their related properties.
+  static MAP_CENTER = {
+    lat: 43,
+    lng: 169,
+  };
 
-const SUZHOU_COORD = {
-  lat: 31.293463,
-  lng: 120.666522,
-  title: 'My Hometown',
-  path: '/suzhou',
-};
+  static SUZHOU_COORD = {
+    lat: 31.293463,
+    lng: 120.666522,
+    title: 'My Hometown',
+    id: 'suzhou',
+  };
 
-const SMITH_COORD = {
-  lat: 42.319266,
-  lng: -72.642447,
-  title: 'Smith College',
-  path: '/smith',
-};
+  static SMITH_COORD = {
+    lat: 42.319266,
+    lng: -72.642447,
+    title: 'Smith College',
+    id: 'smith',
+  };
 
-const RICE_COORD = {
-  lat: 29.717296,
-  lng: -95.399418,
-  title: 'Rice University',
-  path: '/rice',
-};
+  static RICE_COORD = {
+    lat: 29.717296,
+    lng: -95.399418,
+    title: 'Rice University',
+    id: 'rice',
+  };
 
-// The embedded Google Map for my introduction page.
-const map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: MAP_CENTER.lat, lng: MAP_CENTER.lng},
-  zoom: 2,
-});
+  constructor(document) {
+    this.previousSection = undefined;
+    this.document = document;
 
-// All markers for locations meaningful to me.
-// Each marker has corresponding onclick event
-// which updates html content in 'intro-by-location' div.
-const hometownMarker = new google.maps.Marker({
-  position: {lat: SUZHOU_COORD.lat, lng: SUZHOU_COORD.lng},
-  map: map,
-  title: SUZHOU_COORD.title,
-});
+    // The embedded Google Map for my introduction page.
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: MapHandler.MAP_CENTER.lat, lng: MapHandler.MAP_CENTER.lng},
+      zoom: 2,
+    }); 
 
-hometownMarker.addListener('click', async () => {
-  changeContent(SUZHOU_COORD.path);
-});
+    // All markers for locations meaningful to me.
+    // Each marker has corresponding onclick event
+    // which updates html content in 'intro-by-location' div.
+    const hometownMarker = new google.maps.Marker({
+      position: {lat: MapHandler.SUZHOU_COORD.lat, lng: MapHandler.SUZHOU_COORD.lng},
+      map: map,
+      title: MapHandler.SUZHOU_COORD.title,
+    });
 
-const smithCollegeMarker = new google.maps.Marker({
-  position: {lat: SMITH_COORD.lat, lng: SMITH_COORD.lng},
-  map: map,
-  title: SMITH_COORD.title,
-});
+    const smithCollegeMarker = new google.maps.Marker({
+      position: {lat: MapHandler.SMITH_COORD.lat, lng: MapHandler.SMITH_COORD.lng},
+      map: map,
+      title: MapHandler.SMITH_COORD.title,
+    });
 
-smithCollegeMarker.addListener('click', async () => {
-  changeContent(SMITH_COORD.path);
-});
+    const riceUnivMarker = new google.maps.Marker({
+      position: {lat: MapHandler.RICE_COORD.lat, lng: MapHandler.RICE_COORD.lng},
+      map: map,
+      title: MapHandler.RICE_COORD.title,
+    });
 
-const RiceUnivMarker = new google.maps.Marker({
-  position: {lat: RICE_COORD.lat, lng: RICE_COORD.lng},
-  map: map,
-  title: RICE_COORD.title,
-});
+    hometownMarker.addListener('click', 
+                               () => this.changeContent(MapHandler.SUZHOU_COORD.id));
 
-RiceUnivMarker.addListener('click', async () => {
-  changeContent(RICE_COORD.path);
-});
+    smithCollegeMarker.addListener('click', 
+                                   () => this.changeContent(MapHandler.SMITH_COORD.id));
 
-/**
- * The onclick event handler for all markers,
- * which sends request to backend and for corresponding html.
- */
-const changeContent = async (path) => {
-  let content = await fetch(path);
-  document.getElementById('intro-by-place').innerHTML = content;
-};
+    riceUnivMarker.addListener('click', 
+                               () => this.changeContent(MapHandler.RICE_COORD.id));
+  }
+
+  /**
+   * The onclick event handler for all markers,
+   * which sends request to backend and for corresponding html.
+   */
+  changeContent(id) {
+    if (this.previousSection != undefined) {
+      this.document.getElementById(this.previousSection)
+                   .classList
+                   .remove("show-intro");
+    }
+    this.document.getElementById(id)
+                 .classList
+                 .add('show-intro');
+    this.previousSection = id;
+  };
+}
+
+window.onload = () => {
+  const mapHandler = new MapHandler(document);
+}
+
