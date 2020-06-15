29688ef51220a7981b9bcfe48d9a3cf1cd4860cc
Lorraine Lyu <yl191@rice.edu>
2020-06-15 21:02:17 +0000

    adds optional fields

--- portfolio/src/main/java/com/google/sps/data/LoginStatus.java
+++ portfolio/src/main/java/com/google/sps/data/LoginStatus.java
@@ -1,35 +1,39 @@
 package com.google.sps.data;
 
+import java.util.Optional;
+
 /** 
  * The object used to update client with user's log-in status and
  * information to display on client side.
  */
 public class LoginStatus {
 
-  private final Boolean isUserLoggedIn;
-  private final String helperInfo;
+  private final boolean isUserLoggedIn;
+  private final Optional<String> loginUrl;
   private final String logoutUrl;
+  private final Optional<String> username;
 
-  public LoginStatus(Boolean status, String info, String url) {
-      this.isUserLoggedIn = status;
-      this.helperInfo = info;
-      this.logoutUrl = url;
+  public LoginStatus(boolean isUserLoggedIn, Optional<String> loginUrl, String logoutUrl, Optional<String> username) {
+      this.isUserLoggedIn = isUserLoggedIn;
+      this.loginUrl = loginUrl;
+      this.logoutUrl = logoutUrl;
+      this.username = username;
   }
 
   /** 
    * Getter of the login status. 
    * @return the boolean indicating whether the user logged in.
    */
-  public Boolean getStatus() {
+  public boolean getLoginStatus() {
     return this.isUserLoggedIn;
   }
 
   /** 
-   * Getter of information related to user's current status.
-   * @return either the user name or the url for login.
+   * Getter of the login url.
+   * @return the url for login.
    */
-  public String getHelperInfo() {
-    return this.helperInfo;
+  public Optional<String> getLoginUrl() {
+    return this.loginUrl;
   }
 
   /** 
@@ -38,5 +42,13 @@ public class LoginStatus {
    */
   public String getLogoutUrl() {
     return this.logoutUrl;
+  }
+
+  /** 
+   * Getter of username.
+   * @return the username of current user.
+   */
+  public Optional<String> getusername() {
+    return this.username;
   }
 }
